63577215af74889d6cff98d0ac31b805132a5aa9
swolekat <swolecattwitch@gmail.com>
2022-02-13 13:11:37 -0600

    first cut at animation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pulsoid-animation
 Better pulsoid animation so people know it's a heart rate
+
+## How to use
+Get the pulsoid guid for your widget. If you already have everything set up, it will be in the url in your browser source. It'll be after `https://pulsoid.net/widget/view/`
+
+Now use this as your browser source instead `https://swolekat.github.io/pulsoid-animation?pulsoid-id=YOUR_ID_GOES_HERE`
+
---
+++ code.js
@@ -0,0 +1,40 @@
+const params = new URLSearchParams(window.location.search);
+
+const pulsoidId = params.get("pulsoid-id");
+
+const socket = new WebSocket(`wss://ramiel.pulsoid.net/listen/${pulsoidId}`);
+const elementId = 'heart-number';
+const element = document.getElementById(elementId);
+const pulseElementId = 'heart-pulse';
+const pulseElement = document.getElementById(pulseElementId);
+let heartRate = 999;
+let isPulsing = false;
+
+const pulse = () => {
+    isPulsing = true;
+    pulseElement.style.transitionDuration = 0;
+    pulseElement.style.transform = 'scale(0.6)';
+    pulseElement.style.opacity = '1';
+    const beatsPerSecond = heartRate/60;
+    const secondsPerBeat = 1/beatsPerSecond;
+    const msPerBeat = secondsPerBeat * 1000;
+    setTimeout(() => {
+        pulseElement.style.transitionDuration = `${secondsPerBeat}s`;
+        pulseElement.style.transform = 'scale(1)';
+        setTimeout(() => {
+            pulse();
+        }, msPerBeat);
+    }, msPerBeat);
+};
+
+socket.onmessage = (event) => {
+    heartRate = JSON.parse(event.data).data.heartRate;
+    element.innerText = heartRate;
+    if(!isPulsing){
+        pulse();
+    }
+};
+
+socket.onerror = (event) => {
+    console.log(event);
+};
